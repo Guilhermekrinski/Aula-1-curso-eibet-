de2d3f5deb45f3b7639312a410720ef19270c46f
Guilhermekrinski <157639920+Guilhermekrinski@users.noreply.github.com>
2024-03-16 11:30:40 -0300

    Add files via upload

---
+++ pascoa/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Páscoa Solidária</title>
+</head>
+<body>
+    <a href="responsavel.php">Cadastro de responsáveis</a>
+    <a href="padrinho.php">Cadastro de padrinhos</a>
+</body>
+</html>
